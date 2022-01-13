89ff481241a32414fa6feda557ea31e8673a4530
Malay.Varma <Malay.Varma@dell.com>
2022-01-13 15:13:51 +0530

    review comments

--- src/components/select/Select.stories.tsx
+++ src/components/select/Select.stories.tsx
@@ -8,141 +8,123 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  */
 
- import "styles/components/Select.scss";
- import {storiesOf} from "@storybook/react";
- import {action} from "@storybook/addon-actions";
- import {Select, SelectOption, SelectOptionGroup} from "@dellstorage/clarity-react/forms/select";
- 
- storiesOf("Select", module)
-     .add("Basic select", () => (
-         <div>
-             <Select onChange={action("basic change")}>
-                 <SelectOption value="1">One</SelectOption>
-                 <SelectOption value="2">Two</SelectOption>
-                 <SelectOption value="3">Three</SelectOption>
-             </Select>
-         </div>
-     ))
-     .add("Basic select with select option", () => (
-         <div>
-             <Select onChange={action("basic change")}>
-                 <SelectOption value="select">select</SelectOption>
-                 <SelectOption value="1">One</SelectOption>
-                 <SelectOption value="2">Two</SelectOption>
-                 <SelectOption value="3">Three</SelectOption>
-             </Select>
-         </div>
-     ))
-     .add("Select with custom width", () => (
-         <div>
-             <Select onChange={action("basic change")} width="30%" label="Basic Select">
-                 <SelectOption value="1">One</SelectOption>
-                 <SelectOption value="2">Two</SelectOption>
-                 <SelectOption value="3">Three</SelectOption>
-             </Select>
- 
-             <br />
-             {/* <Select onChange={action("basic change")} width="50%" isBoxed label="Boxed Select" className="boxedSelect">
-                 <SelectOption value="1">One</SelectOption>
-                 <SelectOption value="2">Two</SelectOption>
-                 <SelectOption value="3">Three</SelectOption>
-             </Select> */}
-         </div>
-     ))
-     .add("Labels", () => (
-         <div>
-             <Select onChange={action("label select - change")} label="I've got some options">
-                 <SelectOption value="1">One</SelectOption>
-                 <SelectOption value="2">Two</SelectOption>
-                 <SelectOption value="3">Three</SelectOption>
-             </Select>
-         </div>
-     ))
-     .add("Full select display", () => (
-         <div>
-             <Select
-                 onChange={action("full select - change")}
-                 defaultHelperText="You have these choices"
-                 errorHelperText="This field is required!"
-                 label="I've got some options"
-                 defaultValue={"2"}
-             >
-                 <SelectOption value="1">One</SelectOption>
-                 <SelectOption value="2">Two</SelectOption>
-                 <SelectOption value="3">Three</SelectOption>
-             </Select>
-         </div>
-     ))
-     .add("Select with error", () => (
-         <div>
-             <Select
-                 error={true}
-                 errorTitle={"This field is required"}
-                 width="30%"
-                 onBlur={action("select with error and custom width - blur")}
-                 onChange={action("select with error - change")}
-                 defaultHelperText="You have these choices"
-                 errorHelperText="This field is required!"
-                 label="I've got some options"
-             >
-                 <SelectOption value="1">One</SelectOption>
-                 <SelectOption value="2">Two</SelectOption>
-                 <SelectOption value="3">Three</SelectOption>
-             </Select>
- 
-             <Select
-                 error={true}
-                 onBlur={action("select with error - blur")}
-                 onChange={action("select with error - change")}
-                 defaultHelperText="You have these choices"
-                 errorHelperText="This field is required!"
-                 label="I've got some options"
-             >
-                 <SelectOption value="1">One</SelectOption>
-                 <SelectOption value="2">Two</SelectOption>
-                 <SelectOption value="3">Three</SelectOption>
-             </Select>
-         </div>
-     ))
-     .add("Disabled select", () => (
-         <div>
-             <Select onChange={action("Disabled select - change")} disabled={true} label="I've got some options">
-                 <SelectOption value="1">One</SelectOption>
-                 <SelectOption value="2">Two</SelectOption>
-                 <SelectOption value="3">Three</SelectOption>
-             </Select>
-         </div>
-     ))
-     .add("Select Option Group", () => (
-         <div>
-             <Select onChange={action("Option group select - change")}>
-                 <SelectOptionGroup label="Group 1">
-                     <SelectOption value="1">One</SelectOption>
-                     <SelectOption value="2">Two</SelectOption>
-                     <SelectOption value="3">Three</SelectOption>
-                 </SelectOptionGroup>
-                 <SelectOptionGroup label="Group 2">
-                     <SelectOption value="7">Seven</SelectOption>
-                     <SelectOption value="8">Eight</SelectOption>
-                     <SelectOption value="9">Nine</SelectOption>
-                 </SelectOptionGroup>
-             </Select>
-         </div>
-     ))
-     .add("Select Option Group Disabled", () => (
-         <div>
-             <Select onChange={action("Option Group select Disabled - change")}>
-                 <SelectOptionGroup label="Group 1" disabled>
-                     <SelectOption value="1">One</SelectOption>
-                     <SelectOption value="2">Two</SelectOption>
-                     <SelectOption value="3">Three</SelectOption>
-                 </SelectOptionGroup>
-                 <SelectOptionGroup label="Group 2">
-                     <SelectOption value="7">Seven</SelectOption>
-                     <SelectOption value="8">Eight</SelectOption>
-                     <SelectOption value="9">Nine</SelectOption>
-                 </SelectOptionGroup>
-             </Select>
-         </div>
-     ));
- 
+import "styles/components/Select.scss";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { Select, SelectOption, SelectOptionGroup } from "@dellstorage/clarity-react/forms/select";
+
+const renderDefaultSelectOptions = () => (
+    <>
+        <SelectOption value="1">One</SelectOption>
+        <SelectOption value="2">Two</SelectOption>
+        <SelectOption value="3">Three</SelectOption>
+    </>
+)
+
+storiesOf("Select", module)
+    .add("Basic select", () => (
+        <div>
+            <Select onChange={action("basic change")}>
+                {renderDefaultSelectOptions()}
+            </Select>
+        </div>
+    ))
+    .add("Basic select with select option", () => (
+        <div>
+            <Select onChange={action("basic change")}>
+                <SelectOption value="select">select</SelectOption>
+                {renderDefaultSelectOptions()}
+            </Select>
+        </div>
+    ))
+    .add("Select with custom width", () => (
+        <div>
+            <Select onChange={action("basic change")} width="30%" label="Basic Select">
+                {renderDefaultSelectOptions()}
+            </Select>
+
+            <br />
+        </div>
+    ))
+    .add("Select with Labels", () => (
+        <div>
+            <Select onChange={action("label select - change")} label="I've got some options">
+                {renderDefaultSelectOptions()}
+            </Select>
+        </div>
+    ))
+    .add("Full select display", () => (
+        <div>
+            <Select
+                onChange={action("full select - change")}
+                defaultHelperText="You have these choices"
+                errorHelperText="This field is required!"
+                label="I've got some options"
+                defaultValue={"2"}
+            >
+                {renderDefaultSelectOptions()}
+            </Select>
+        </div>
+    ))
+    .add("Select with error", () => (
+        <div>
+            <Select
+                error={true}
+                errorTitle={"This field is required"}
+                width="30%"
+                onBlur={action("select with error and custom width - blur")}
+                onChange={action("select with error - change")}
+                defaultHelperText="You have these choices"
+                errorHelperText="This field is required!"
+                label="I've got some options"
+            >
+                {renderDefaultSelectOptions()}
+            </Select>
+
+            <Select
+                error={true}
+                onBlur={action("select with error - blur")}
+                onChange={action("select with error - change")}
+                defaultHelperText="You have these choices"
+                errorHelperText="This field is required!"
+                label="I've got some options"
+            >
+                {renderDefaultSelectOptions()}
+            </Select>
+        </div>
+    ))
+    .add("Disabled select", () => (
+        <div>
+            <Select onChange={action("Disabled select - change")} disabled={true} label="I've got some options">
+                {renderDefaultSelectOptions()}
+            </Select>
+        </div>
+    ))
+    .add("Select Option Group", () => (
+        <div>
+            <Select onChange={action("Option group select - change")}>
+                <SelectOptionGroup label="Group 1">
+                    {renderDefaultSelectOptions()}
+                </SelectOptionGroup>
+                <SelectOptionGroup label="Group 2">
+                    <SelectOption value="7">Seven</SelectOption>
+                    <SelectOption value="8">Eight</SelectOption>
+                    <SelectOption value="9">Nine</SelectOption>
+                </SelectOptionGroup>
+            </Select>
+        </div>
+    ))
+    .add("Select Option Group Disabled", () => (
+        <div>
+            <Select onChange={action("Option Group select Disabled - change")}>
+                <SelectOptionGroup label="Group 1" disabled>
+                    {renderDefaultSelectOptions()}
+                </SelectOptionGroup>
+                <SelectOptionGroup label="Group 2">
+                    <SelectOption value="7">Seven</SelectOption>
+                    <SelectOption value="8">Eight</SelectOption>
+                    <SelectOption value="9">Nine</SelectOption>
+                </SelectOptionGroup>
+            </Select>
+        </div>
+    ));
