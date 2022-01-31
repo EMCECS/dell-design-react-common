3e9aff08b66027061f0fcedb77d09388ca007fdd
Priyadarshni Sivakumaran <priyadarshni.sivakumaran@dell.com>
2022-01-31 19:04:47 +0530

    [OBSDEF-13994] - Wrapper for Datalist component

---
+++ src/components/datalist/Datalist.stories.tsx
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) 2022 Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+import * as React from "react";
+import {storiesOf} from "@storybook/react";
+import {DataList, DataListOption} from "@dellstorage/clarity-react/forms/datalist/DataList"
+import "styles/components/DataList.scss";
+import {action} from "@storybook/addon-actions";
+
+storiesOf("DataList", module)
+    .add("Basic DataList", () => (
+        <div>
+            <DataList>
+                <DataListOption value="Item1" />
+                <DataListOption value="Item2" />
+                <DataListOption value="Item3" />
+            </DataList>
+        </div>
+    ))
+    .add("With place holder text", () => (
+        <div>
+            <DataList placeHolder="No label">
+                <DataListOption value="Item1" />
+                <DataListOption value="Item2" />
+                <DataListOption value="Item3" />
+            </DataList>
+        </div>
+    ))
+    .add("With Error", () => (
+        <div>
+            <DataList
+                placeHolder="No label"
+                required={true}
+                isError={true}
+                errorText="This field is reuired"
+                errorTitle="This field is required"
+            >
+                <DataListOption value="Item1" />
+                <DataListOption value="Item2" />
+                <DataListOption value="Item3" />
+            </DataList>
+        </div>
+    ))
+    .add("With helper text", () => (
+        <div>
+            <DataList placeHolder="No label" helperText="Select any option or create one">
+                <DataListOption value="Item1" />
+                <DataListOption value="Item2" />
+                <DataListOption value="Item3" />
+            </DataList>
+        </div>
+    ))
+    .add("With custom width and label", () => (
+        <div>
+            <DataList
+                placeHolder="No label"
+                helperText="Select any option or create one"
+                label="Select Item"
+                style={{width: "50%"}}
+            >
+                <DataListOption value="Item1" />
+                <DataListOption value="Item2" />
+                <DataListOption value="Item3" />
+            </DataList>
+        </div>
+    ))
+    .add("With defaultSelected Value", () => (
+        <div>
+            <DataList
+                placeHolder="No label"
+                helperText="Select any option or create one"
+                label="Select Item"
+                defaultValue="Item1"
+            >
+                <DataListOption value="Item1" />
+                <DataListOption value="Item2" />
+                <DataListOption value="Item3" />
+            </DataList>
+        </div>
+    ))
+    .add("DataList with Debounce true", () => (
+        <div>
+            <DataList
+                placeHolder="No label"
+                helperText="Select any option or create one"
+                label="Select Item"
+                onChange={action("changed: after 5 secs")}
+                debounce={true}
+                debounceTime={1000}
+            >
+                <DataListOption value="Item1" />
+                <DataListOption value="Item2" />
+                <DataListOption value="Item3" />
+            </DataList>
+        </div>
+    ))
+    .add("DataList with Debounce OFF by default", () => (
+        <div>
+            <DataList
+                placeHolder="No label"
+                helperText="Select any option or create one"
+                label="Select Item"
+                onChange={action("changed: Immediately")}
+            >
+                <DataListOption value="Item1" />
+                <DataListOption value="Item2" />
+                <DataListOption value="Item3" />
+            </DataList>
+        </div>
+    ));
