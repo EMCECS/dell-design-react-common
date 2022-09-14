6b4d907de43cf5e16f8462ef3b83e2c8130777f0
Kruti <Kruti.Saklecha@dell.com>
2022-09-14 10:23:52 +0530

    Removed other subfeatures of DataGrid, added functional DataGrid

--- src/components/DataGridWithInfiniteScroll/DataGridInfiniteScroll.stories.tsx
+++ src/components/DataGridWithInfiniteScroll/DataGridInfiniteScroll.stories.tsx
@@ -9,23 +9,11 @@
  */
 
 import { storiesOf } from "@storybook/react";
-import DataGridWithInfiniteScroll, {
-    GridSelectionType,
-} from "./DataGridWithInfiniteScroll";
+import DataGridWithInfiniteScroll from "components/DataGridWithInfiniteScroll/DataGridWithInfiniteScroll";
 import {
-    columns,
     columnsData,
-    columnsExpansion,
     DATA,
-    dataExpansion,
-    detailColumns,
-    DETAILDATA,
-    detailDataProps,
-    ExpandedComponent,
-    filterComponent,
-    getColumnNameAndOrder,
 } from "components/DataGridWithInfiniteScroll/DatagridInfiniteScrollMockData";
-import DataTable from "react-data-table-component";
 import "styles/components/DataGridWithInfinteScroll.scss";
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -35,80 +23,4 @@ storiesOf("Data Grid with Infinite Scroll", module)
             <DataGridWithInfiniteScroll rows={DATA.rows} columns={columnsData} />
         </div>
     ))
-    .add("Grid with Multiselect", () => (
-        <div>
-            <DataGridWithInfiniteScroll
-                rows={DATA.rows}
-                columns={columnsData}
-                selectionType={GridSelectionType.MULTI}
-            />
-        </div>
-    ))
-    .add("Grid with Single Select", () => (
-        <div>
-            <DataGridWithInfiniteScroll
-                rows={DATA.rows}
-                columns={columnsData}
-                selectionType={GridSelectionType.SINGLE}
-            />
-        </div>
-    ))
-    .add("Grid with Sorting", () => (
-        <div>
-            <DataGridWithInfiniteScroll
-                rows={DATA.rows}
-                columns={columnsData}
-                isSorting={true}
-                callbackForColumnName={getColumnNameAndOrder}
-            />
-        </div>
-    ))
-    .add("Grid with Filter", () => (
-        <div>
-            <DataGridWithInfiniteScroll
-                rows={DATA.rows}
-                columns={columnsData}
-                isFilter={true}
-                filterData={filterComponent()}
-            />
-        </div>
-    ))
-    .add("Grid with Expandable Row", () => (
-        <div>
-            <DataTable
-                columns={columnsExpansion}
-                data={dataExpansion}
-                expandableRows
-                expandableRowsComponent={ExpandedComponent}
-            />
-        </div>
-    ))
-    .add("Grid with Column Hide/Show", () => (
-        <div>
-            <DataGridWithInfiniteScroll
-                rows={DETAILDATA}
-                columns={detailColumns}
-                showColumnSelect={true}
-                defaultColumnHeader={"Status"}
-            />
-        </div>
-    ))
-    .add("Grid with Detail Panel", () => (
-        <div>
-            <DataGridWithInfiniteScroll
-                rows={DETAILDATA}
-                columns={detailColumns}
-                showDetailPanel={true}
-                detailDataProps={detailDataProps}
-            />
-        </div>
-    ))
-    .add("Empty Datagrid", () => (
-        <div>
-            <DataGridWithInfiniteScroll
-                rows={[]}
-                columns={columns}
-                style={{ height: "70vh" }}
-            />
-        </div>
-    ));
+
