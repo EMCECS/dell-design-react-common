ab3482d61f9b33ed787085b3fb6abce3987d852e
Bhawani Singh <bhati.bhawani@dell.com>
2022-09-16 17:41:28 +0530

    [OBSDEF-19680] Fix PR comments

--- src/components/tooltip/ToolTip.stories.tsx
+++ src/components/tooltip/ToolTip.stories.tsx
@@ -12,36 +12,36 @@ import {storiesOf} from "@storybook/react";
 import {ToolTip, ToolTipDirection, ToolTipSize} from "@dellstorage/clarity-react/forms/tooltip/ToolTip";
 import "styles/components/ToolTip.scss";
 
+const containerStyles = {
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    height: "80vh",
+    width: "75vw",
+};
+
 storiesOf("ToolTip", module)
-    .add("Tooltip sizes", () => (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "space-around",
-                alignItems: "center",
-                height: "80vh",
-                width: "75vw",
-            }}
-        >
+    .add("Tooltip Sizes", () => (
+        <div style={containerStyles}>
             <div>
                 <ToolTip size={ToolTipSize.EXTRA_SMALL} isSolidIcon>
                     Content size: 16px
                 </ToolTip>
                 Extra Small
             </div>
-            <div style={{marginLeft: "20px"}}>
+            <div>
                 <ToolTip size={ToolTipSize.SMALL} isSolidIcon iconSize={20}>
                     Content size: 20px
                 </ToolTip>
                 Small
             </div>
-            <div style={{marginLeft: "20px"}}>
+            <div>
                 <ToolTip size={ToolTipSize.MEDIUM} isSolidIcon iconSize={24}>
                     Content size: 24px
                 </ToolTip>
                 Medium
             </div>
-            <div style={{marginLeft: "20px"}}>
+            <div>
                 <ToolTip size={ToolTipSize.LARGE} isSolidIcon iconSize={30}>
                     Content size: 30px
                 </ToolTip>
@@ -49,59 +49,51 @@ storiesOf("ToolTip", module)
             </div>
         </div>
     ))
-    .add("Tooltip directions", () => (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "space-around",
-                alignItems: "center",
-                height: "80vh",
-                width: "75vw",
-            }}
-        >
-            <div style={{float: "left", marginLeft: "20px"}}>
+    .add("Tooltip Directions", () => (
+        <div style={containerStyles}>
+            <div>
                 <ToolTip size={ToolTipSize.MEDIUM} isSolidIcon>
                     Top
                 </ToolTip>
                 Top
             </div>
-            <div style={{float: "left", marginLeft: "20px"}}>
+            <div>
                 <ToolTip size={ToolTipSize.MEDIUM} isSolidIcon direction={ToolTipDirection.BOTTOM}>
                     Bottom
                 </ToolTip>
                 Bottom
             </div>
-            <div style={{float: "left", marginLeft: "20px"}}>
+            <div>
                 <ToolTip size={ToolTipSize.MEDIUM} isSolidIcon direction={ToolTipDirection.TOP_RIGHT}>
                     Top Right
                 </ToolTip>
                 Top Right
             </div>
-            <div style={{float: "left", marginLeft: "20px"}}>
+            <div>
                 <ToolTip size={ToolTipSize.MEDIUM} isSolidIcon direction={ToolTipDirection.BOTTOM_RIGHT}>
                     Bottom Right
                 </ToolTip>
                 Bottom Right
             </div>
-            <div style={{float: "left", marginLeft: "20px"}}>
+            <div>
                 <ToolTip size={ToolTipSize.MEDIUM} isSolidIcon direction={ToolTipDirection.RIGHT}>
                     Right
                 </ToolTip>
                 Right
             </div>
-            <div style={{float: "left", marginLeft: "20px"}}>
+            <div>
                 <ToolTip size={ToolTipSize.MEDIUM} isSolidIcon direction={ToolTipDirection.TOP_LEFT}>
                     Top Left
                 </ToolTip>
                 Top Left
             </div>
-            <div style={{float: "left", marginLeft: "20px"}}>
+            <div>
                 <ToolTip size={ToolTipSize.MEDIUM} isSolidIcon direction={ToolTipDirection.BOTTOM_LEFT}>
                     Bottom Left
                 </ToolTip>
                 Bottom Left
             </div>
-            <div style={{float: "left", marginLeft: "20px"}}>
+            <div>
                 <ToolTip size={ToolTipSize.MEDIUM} isSolidIcon direction={ToolTipDirection.LEFT}>
                     Left
                 </ToolTip>
@@ -109,16 +101,8 @@ storiesOf("ToolTip", module)
             </div>
         </div>
     ))
-    .add("Tooltip customization", () => (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "space-around",
-                alignItems: "center",
-                height: "80vh",
-                width: "75vw",
-            }}
-        >
+    .add("Tooltip Customization", () => (
+        <div style={containerStyles}>
             <div>
                 <ToolTip>Content size: 20px</ToolTip>
                 Default ToolTip
@@ -127,29 +111,29 @@ storiesOf("ToolTip", module)
                 <ToolTip isSolidIcon>Content size: 20px</ToolTip>
                 Solid ToolTip
             </div>
-            <div style={{float: "left"}}>
+            <div>
                 <ToolTip size={ToolTipSize.EXTRA_SMALL} isSolidIcon>
-                    Lorem
+                    Content size: 16px
                 </ToolTip>
-                Extra Small
+                Extra Small ToolTip
             </div>
-            <div style={{}}>
+            <div>
                 <ToolTip size={ToolTipSize.SMALL} isSolidIcon iconSize={20}>
-                    Lorem ipsum sit
+                    Content size: 20px
                 </ToolTip>
-                Small
+                Small ToolTip
             </div>
-            <div style={{float: "left", marginLeft: "20px"}}>
+            <div>
                 <ToolTip size={ToolTipSize.MEDIUM} isSolidIcon iconSize={24} shape="check-circle">
-                    Loren ipsum dolor sit amet, ipsum
+                    Content size: 24px
                 </ToolTip>
-                Medium
+                Medium ToolTip
             </div>
-            <div style={{float: "left", marginLeft: "20px"}}>
+            <div>
                 <ToolTip size={ToolTipSize.LARGE} iconSize={30} shape="exclamation-triangle" style={{color: "orange"}}>
-                    Loren ipsum dolor sit amet, consectetur adipisicing elit
+                    Content size: 30px
                 </ToolTip>
-                Large
+                Large ToolTip
             </div>
         </div>
     ));
