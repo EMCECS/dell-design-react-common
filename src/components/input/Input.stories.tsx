2a8661ba9e1f5ba80739dca3be13a98ba3d78a6d
Muralidaran <Manaswini.Muralidara@dell.com>
2022-09-23 13:33:48 +0530

    [OBSDEF-19957]Fix CSS paddings on clr-select

--- src/components/input/Input.stories.tsx
+++ src/components/input/Input.stories.tsx
@@ -15,34 +15,22 @@ import {storiesOf} from "@storybook/react";
 import {action} from "@storybook/addon-actions";
 import {Input} from "@dellstorage/clarity-react/forms/input/Input";
 
+const divClassname = "clr-row clr-justify-content-start field-comp-margin";
+
 storiesOf("Input", module)
     .add("Input Box", () => (
         <div>
-            <Input
-                name="somevalue"
-                placeholder="Input Textbox"
-                onChange={action("changed")}
-            />
+            <Input name="somevalue" placeholder="Input Textbox" onChange={action("changed")} />
         </div>
     ))
     .add("input box with label", () => (
         <div>
-            <Input
-                name="somevalue"
-                label="Label 1"
-                placeholder="Input Textbox"
-                onChange={action("changed")}
-            />
+            <Input name="somevalue" label="Label 1" placeholder="Input Textbox" onChange={action("changed")} />
         </div>
     ))
     .add("input box with label and helper text", () => (
         <div>
-            <Input
-                name="somevalue"
-                label="Label 1"
-                helperText="Input Textbox"
-                onChange={action("changed")}
-            />
+            <Input name="somevalue" label="Label 1" helperText="Input Textbox" onChange={action("changed")} />
         </div>
     ))
     .add("input box with validation", () => (
@@ -81,12 +69,16 @@ storiesOf("Input", module)
     ))
     .add("input box number", () => (
         <div>
-            <Input
-                name="somevalue"
-                label="Label 1"
-                type="number"
-                placeholder="Number"
-                onChange={action("changed")}
-            />
+            <Input name="somevalue" label="Label 1" type="number" placeholder="Number" onChange={action("changed")} />
+        </div>
+    ))
+    .add("form field - input box with label", () => (
+        <div className={divClassname}>
+            <span className="clr-col-1 clr-align-self-center" style={{marginTop: "1.9rem"}}>
+                {"Label"}
+            </span>
+            <div className="clr-col clr-align-self-center">
+                <Input name="somevalue" type="number" placeholder="Number" onChange={action("changed")} />
+            </div>
         </div>
     ));
